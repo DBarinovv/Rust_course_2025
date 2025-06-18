1b71866119d40e76a4abeae1aefbaa8ac82e4f26
Утегенов Артем <utegartem@MacBook-Pro-Utegenov.local>
2025-06-18 23:40:41 +0300

    Add comm

--- udos/comm/src/main.rs
+++ udos/comm/src/main.rs
@@ -1,8 +1,31 @@
 #![forbid(unsafe_code)]
 
-// TODO: your code goes here.
+use std::{fs::File, io::BufRead, io::BufReader};
+use std::collections::HashSet;
+use std::io;
+
+fn read_lines(path: &str) -> io::Result<HashSet<String>> {
+    let file = File::open(path).unwrap();
+    let reader = BufReader::new(file);
+    reader.lines().collect()
+}
 
 fn main() {
-    // TODO: your code goes here.
-    unimplemented!()
+
+    let args = std::env::args().collect::<Vec<String>>();
+
+    let path1 = &args[1];
+    let path2 = &args[2];
+
+    let lines_file1 = read_lines(path1).unwrap();
+    let lines_file2 = read_lines(path2).unwrap();
+    
+    for line_file1 in &lines_file1 {
+        for line_file2 in &lines_file2 {
+            if *line_file2 == *line_file1 {
+                println!("{}", *line_file1);
+            }
+        }
+    }
+
 }
