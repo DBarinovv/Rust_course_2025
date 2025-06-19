138e1453d959d737620b7a17ef14e40d7ff23eaf
Утегенов Артем <utegartem@MacBook-Pro-Utegenov.local>
2025-06-19 16:09:10 +0300

    Add min_queue

--- udos/min-queue/src/lib.rs
+++ udos/min-queue/src/lib.rs
@@ -4,42 +4,52 @@ use std::collections::VecDeque;
 
 #[derive(Default)]
 pub struct MinQueue<T> {
-    // TODO: your code goes here.
+    queue: VecDeque<T>,
+    min: VecDeque<T>,
 }
 
 impl<T: Clone + Ord> MinQueue<T> {
     pub fn new() -> Self {
-        // TODO: your code goes here.
-        unimplemented!()
+        MinQueue {
+            queue: VecDeque::new(),
+            min: VecDeque::new(),
+        }
     }
 
     pub fn push(&mut self, val: T) {
-        // TODO: your code goes here.
-        unimplemented!()
+        while let Some(back) = self.min.back() {
+            if back <= &val {
+                break;
+            }
+            self.min.pop_back();
+        }
+        self.min.push_back(val.clone());
+        self.queue.push_back(val);
     }
 
     pub fn pop(&mut self) -> Option<T> {
-        // TODO: your code goes here.
-        unimplemented!()
+        let val = self.queue.pop_front()?;
+
+        if self.min.front() == Some(&val) {
+            self.min.pop_front();
+        }
+
+        Some(val)
     }
 
     pub fn front(&self) -> Option<&T> {
-        // TODO: your code goes here.
-        unimplemented!()
+        self.queue.front()
     }
 
     pub fn min(&self) -> Option<&T> {
-        // TODO: your code goes here.
-        unimplemented!()
+        self.min.front()
     }
 
     pub fn len(&self) -> usize {
-        // TODO: your code goes here.
-        unimplemented!()
+        self.queue.len()
     }
 
     pub fn is_empty(&self) -> bool {
-        // TODO: your code goes here.
-        unimplemented!()
+        self.queue.is_empty()
     }
 }
